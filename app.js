f3f02d014b71da710b5c9673df4f8b23f1b326a2
rajkumar963 <rajkumarsaw597@gmail.com>
2024-02-07 22:50:18 +0530

    first commit

--- app.js
+++ app.js
@@ -1,38 +1,6 @@
+let section= document.querySelector('section');
+let navlinks= document.querySelectorAll('header nav a');
 
-function loading(){
-    var t1=gsap.timeline();
-t1.to("#yellow", {
-    top:"-100%",
-    delay:0.5,
-    duration:1.5,
-    ease:"expo.out"
-})
-t1.from("#yellow1", {
-    top:"100%",
-    delay:0.6,
-    duration:0.9,
-    ease:"expo.out" 
-},
-"anim"
-);
+window.onscroll= ()=>{
     
-t1.to("#loader h1",{
-    delay:0.6,
-    duration:0.7,
-   color:"black",
-},"anim")
-
-t1.to("#loader",{
-    display:"none"
-})
-t1.to("#loader",{
-    opacity:0
-})
 }
-loading();
-
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('#main'),
-    smooth: true,
-//lerp: 0.02
-});
--- app1.js
+++
@@ -1,6 +0,0 @@
-let section= document.querySelector('section');
-let navlinks= document.querySelectorAll('header nav a');
-
-window.onscroll= ()=>{
-    
-}
